391a8d6d53ce5167f070843fb9736e69cb3b2c62
Cristhian-L-Q-N <tecn0crisis0@gmail.com>
2023-09-21 23:16:26 -0300

    add module AppBar

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,9 @@
+
+import React, { Fragment } from 'react';
+import './styles/index';
+
+const AppBar = (props) => {
+    return <Fragment></Fragment>;
+};
+
+export default AppBar
---
+++ src/components/AppBar/AppBar.stories.jsx
@@ -0,0 +1,11 @@
+
+import React, {Fragment} from 'react';
+import AppBar from './AppBar';
+
+export default {title: 'Component|AppBar'};
+
+export const AppBarExample = () => (
+    <Fragment>
+        <AppBar />
+    </Fragment>
+);
