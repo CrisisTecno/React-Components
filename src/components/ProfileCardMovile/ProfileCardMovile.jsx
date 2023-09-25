756ca82e3fba6846cd62d1e897643705fc96356f
Cristhian-L-Q-N <tecn0crisis0@gmail.com>
2023-09-24 22:11:42 -0300

    add ProfileCardMobile

---
+++ src/components/ProfileCardMovile/ProfileCardMovile.jsx
@@ -0,0 +1,9 @@
+
+import React, { Fragment } from 'react';
+import './styles/index';
+
+const ProfileCardMovile = (props) => {
+    return <Fragment></Fragment>;
+};
+
+export default ProfileCardMovile
---
+++ src/components/ProfileCardMovile/index.js
@@ -0,0 +1,3 @@
+
+import ProfileCardMovile from './ProfileCardMovile';
+export default ProfileCardMovile;    
